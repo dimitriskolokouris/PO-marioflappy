0c86fc54f36d1dcffda11b0c17d37a64afe749b9
Dimitris Kolokouris <95089320+dimitriskolokouris@users.noreply.github.com>
2022-09-21 12:46:20 +0000

    afbeeldingen toegevoegd en bird (mario) stuitert

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-g
+var yVal;
+var acceleration;
+var snelheid;
+var mass;
+
+
+
+function setup() {
+  createCanvas(640, 360);
+  yVal = 0;
+  snelheid = 0;
+  mass = 50;
+  floppy = loadImage("mario.png");
+  acceleration = mass * 0.01;
+}
+
+function game() {
+
+
+  snelheid += acceleration;
+  yVal += snelheid;
+  image(floppy, width / 2, yVal, mass + 20, mass - 10);
+
+
+  if (yVal > height - mass / 2) {
+    snelheid *= -0.6;
+    yVal = height - mass / 2;
+  }
+}
+
+function draw() {
+  game()
+}
